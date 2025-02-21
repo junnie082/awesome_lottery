526cc9ac7674ac29876cc31ee1f756a45770dc03
junnie082 <junnie082@gmail.com>
2025-02-21 16:32:30 +0900

    fix: fixed errors on Dashboard functions

--- members/models.py
+++ members/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Member(models.Model):
-    name = models.CharField(unique=True, max_length=100)
+    name = models.CharField(max_length=100)
     time_Choices = (('230', '230'), ('330', '330'), ('430', '430'), ('530', '530'), ('630', '630'))
     level_Choices = (('A', 'A'), ('A+', 'A+'), ('B', 'B'), ('B+', 'B+'), ('C', 'C'), ('C+', 'C+'))
     mem_time = models.CharField(max_length=100, choices=time_Choices)
     mem_level = models.CharField(max_length=100, choices=level_Choices)
-    total_points = models.IntegerField(default=0, blank=True, null=True)
+    total_points = models.FloatField(default=0, blank=True, null=True)
 
     def __str__(self):
         return self.name
